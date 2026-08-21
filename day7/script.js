f7f1274ccaf15c35def38e8155fd27445b463089
Krishna Garg <kgarg8320@gmail.com>
2026-08-21 10:17:27 +0530

    main

---
+++ day7/script.js
@@ -0,0 +1,16 @@
+const root = ReactDOM.createRoot(document.getElementById("root"));
+const ReactComponent = () => {
+    return (<div>
+
+    <div>Product-01</div>
+    <div>Product-02</div>
+    <div>Product-03</div>
+    <div>Product-04</div>
+    <div>Product-05</div>
+    <div>Product-06</div>
+    <div>Product-07</div>
+</div>)
+}
+
+const reactelement = <ReactComponent />  
+root.render(reactelement);
---
+++ index.js
@@ -0,0 +1,8 @@
+const root = ReactDom.createRoot(document.getElementById("root"))
+const div= <><div>
+    <span>Content 01</span>
+</div>
+    <div>
+        <span>Content 02</span>
+    </div></>
+    root.render(div);
